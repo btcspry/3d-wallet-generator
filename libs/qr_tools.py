0572cdf230e19af3792c984fddfb7e5ffbb5f9b3
btcspry <btcspry@bitforwarder.com>
2015-06-19 13:30:57 -0600

    Create QR Tools

---
+++ libs/qr_tools.py
@@ -0,0 +1,31 @@
+import pyqrcode # sudo pip install pyqrcode
+
+def getQRArray(text, errorCorrection):
+	""" Takes in text and errorCorrection (letter), returns 2D array of the QR code"""
+	# White is True (1)
+	# Black is False (0)
+	# ECC: L7, M15, Q25, H30
+
+	# Create the object
+	qr = pyqrcode.create(text, error=errorCorrection)
+
+	# Get the terminal representation and split by lines
+	plainOut = qr.terminal().split("\n")
+
+	# Initialize the output 2D list
+	out = []
+
+	for line in plainOut:
+		thisOut = []
+		for char in line:
+			if char == u'7':
+				# This is white
+				thisOut.append(1)
+			elif char == u'4':
+				# This is black, it's part of the u'49'
+				thisOut.append(0)
+		# Finally add everything to the output
+		out.append(thisOut)
+
+	# Everything is done, return the qr code list
+	return out
