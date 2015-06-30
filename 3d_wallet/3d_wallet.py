e0c184cc0b98024584afa541d907d9289436ff96
btcspry <btcspry@github.com>
2015-06-30 13:45:19 -0600

    Moved files for PyPi submission
    Also moved scad/round... into the 3d_wallet.py file (to shrink imports)

--- 3d_wallet/3d_wallet.py
+++ 3d_wallet/3d_wallet.py
@@ -99,10 +99,13 @@ scadOutputs = [] # Generated from loop for each wallet (different addresses)
 
 # Include some modules at the beginning
 masterSCAD += "// Import some modules\n"
-moduleFile = open("scad/roundCornersCube.scad","r")
-masterSCAD += moduleFile.read()
-masterSCAD += "\n\n"
-moduleFile.close()
+masterSCAD += """
+$fn=100;
+module createMeniscus(h,radius)difference(){translate([radius/2+0.1,radius/2+0.1,0]){cube([radius+0.2,radius+0.1,h+0.2],center=true);}cylinder(h=h+0.2,r=radius,center=true);}
+module roundCornersCube(x,y,z)translate([x/2,y/2,z/2]){difference(){r=((x+y)/2)*0.052;cube([x,y,z],center=true);translate([x/2-r,y/2-r]){rotate(0){createMeniscus(z,r);}}translate([-x/2+r,y/2-r]){rotate(90){createMeniscus(z,r);}}translate([-x/2+r,-y/2+r]){rotate(180){createMeniscus(z,r);}}translate([x/2-r,-y/2+r]){rotate(270){createMeniscus(z,r);}}}}
+""" # The rounding corners modules for creating a rounded rectangle
+
+masterSCAD += "\n"
 
 # Draw the main prism
 if args.roundCorners:
