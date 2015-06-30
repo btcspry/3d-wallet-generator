3dd4f73746f20f459a2ac2e51a29421266de7596
btcspry <btcspry@github.com>
2015-06-30 13:31:54 -0600

    Updated setup.py and renamed files

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
-  name = '3d-wallet-gen',
-  packages = ['3d-wallet-gen'], # this must be the same as the name above
+  name = '3d_wallet',
+  packages = ['3d_wallet'], # this must be the same as the name above
   version = '0.1',
   description = 'A tool to help you design and export 3D-printable wallets',
   author = 'BTC Spry',
@@ -9,5 +9,5 @@ setup(
   url = 'https://github.com/btcspry/3d-wallet-generator',
   download_url = 'https://github.com/btcspry/3d-wallet-generator/tarball/0.1',
   keywords = ['bitcoin','litecoin','dogecoin','wallet','3d printer','cryptocurrency','altcoin','money'],
-  classifiers = [],
+  classifiers = ["Programming Language :: Python :: 3","License :: OSI Approved :: MIT License","Operating System :: OS Independent","Intended Audience :: End Users/Desktop","Environment :: Console","Development Status :: 4 - Beta","Topic :: Utilities"],
 )
