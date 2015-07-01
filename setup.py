3284f5847a830a10d9fd9b691200fd61d36e21c5
btcspry <btcspry@github.com>
2015-07-01 13:30:49 -0600

    Removed bin/.... line in setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,13 @@ long_description = open('README.rst').read()
 setup(
     name = '3d-wallet-generator',
     packages = ['3d_wallet'], # this must be the same as the name above
-    version = '0.1.10',
+    version = '0.1.11',
     description = 'A tool to help you design and export 3D-printable wallets',
     long_description=long_description,
     author = 'BTC Spry',
     author_email = 'btcspry@bitforwarder.com',
     url = 'https://github.com/btcspry/3d-wallet-generator',
     install_requires=["bitcoin>=1.1.29","PyQrCode>=1.1"],
-    scripts=['bin/3dwallet'],
     keywords = ['bitcoin','litecoin','dogecoin','wallet','3d printer','cryptocurrency','altcoin','money'],
     classifiers = ["Programming Language :: Python :: 3","License :: OSI Approved :: MIT License","Operating System :: OS Independent","Intended Audience :: End Users/Desktop","Environment :: Console","Development Status :: 4 - Beta","Topic :: Utilities"],
     entry_points={
